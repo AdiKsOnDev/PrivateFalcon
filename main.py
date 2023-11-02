179c0a22cc6512709dabf1c31702b42c2be52539
AdiKsOnDev <adilalizade13@gmail.com>
2023-11-02 18:46:44 +0400

    Changed max_new_tokens

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 from chromadb.config import Settings
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from transformers import AutoTokenizer, pipeline
 
 if not load_dotenv():
     print("Could not load .env file or it is empty.")
@@ -30,43 +30,28 @@ def main():
     embeddings = HuggingFaceEmbeddings(model_name=EMBEDDINGS_MODEL)
     chroma_client = chromadb.PersistentClient(settings=CHROMA_SETTINGS, path=DB_DIRECTORY)
 
-    # model_id = "mistralai/Mistral-7B-v0.1"
     model_id = "tiiuae/falcon-7b"
 
     data = Chroma(persist_directory=DB_DIRECTORY, embedding_function=embeddings, client_settings=CHROMA_SETTINGS, client=chroma_client)
     retriever = data.as_retriever(search_kwargs={"k": SOURCE_CHUNKS})
     tokenizer = AutoTokenizer.from_pretrained(model_id)
-    # model = AutoModelForCausalLM.from_pretrained(model_id)
     print("Retriever")
-
-    # llm = HuggingFacePipeline.from_model_id(
-	# 	model_id=model_id,
-	# 	task="text-generation"
-	# )
 
     pipe = pipeline(
         model=model_id,
         tokenizer=tokenizer,
         torch_dtype=torch.bfloat16,
         device_map="auto",
-        max_new_tokens=50
+        max_new_tokens=200
         )
     llm = HuggingFacePipeline(pipeline=pipe)
 
-    # hugging_pipeline = transformers.pipeline(
-    #     "text-generation",
-    #     model=model,
-    #     tokenizer=tokenizer,
-    #     torch_dtype=torch.bfloat16,
-    #     trust_remote_code=True,
-    #     device_map="auto",
-    # )
     print("Made a pipeline")
     
     retrieval = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
 
+    print("Entering loop")
     while True:
-        print("Entering loop")
         
         query = input("\nEnter a query: ")
         if query == "exit":
@@ -82,15 +67,11 @@ def main():
         end = time.time()
 
         # Print the result
-        print("\n\n> Question:")
+        print("\n\n--> Question:")
         print(query)
-        print(f"\n> Answer (took {round(end - start, 2)} s.):")
-        print(answer)
 
-        # # Print the relevant sources used for the answer
-        # for document in docs:
-        #     print("\n> " + document.metadata["source"] + ":")
-        #     print(document.page_content)
+        print(f"\n--> Answer (took {round(end - start, 2)} s.):")
+        print(answer)
 
 if __name__ == "__main__":
     main()
